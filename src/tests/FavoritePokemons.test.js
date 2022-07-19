7676b97311aa74d4f0538e3e1cff5b0428226905
Jessy Cristina <jessyaraujo395@gmail.com>
2022-07-19 17:55:14 -0300

    requisito 3 concluido

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+test(`Se é exibida na tela a mensagem No favorite pokemon found, 
+caso a pessoa não tenha pokémons favoritos;`, () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/favorites');
+  console.log(screen.logTestingPlaygroundURL());
+  const noPokemons = screen.getByText(/no favorite pokemon found/i);
+  expect(noPokemons).toBeInTheDocument();
+});

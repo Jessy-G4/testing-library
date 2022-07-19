b306941977e07ebb611416a5bc2c950512693e89
Jessy Cristina <jessyaraujo395@gmail.com>
2022-07-19 18:19:14 -0300

    requisito 4 concluido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+test(`se a página contém um heading h2 com o texto
+ Page requested not found;`, () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/matheusGoyas');
+  const h2 = screen.getByRole('heading', {
+    name: /page requested not found/i,
+  });
+  expect(h2).toBeInTheDocument();
+});
+
+test(`se a página contém um heading h2 com o texto
+ Page requested not found;`, () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/matheusGoyas');
+  const img = screen.getByRole('img', {
+    name: /pikachu crying because the page requested was not found/i,
+  });
+  expect(img).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
